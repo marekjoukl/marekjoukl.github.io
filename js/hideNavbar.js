740b48d840ca19c088db6e934587740cb354b0e6
Marek Joukl <jouklmarek@seznam.cz>
2024-04-26 16:34:01 +0200

    feat: hide navbar on tablets and phones

---
+++ js/hideNavbar.js
@@ -0,0 +1,21 @@
+const navbar = document.getElementById("nav");
+let prevScrollPos = window.scrollY;
+
+function handleScroll() {
+  let currentScrollPos = window.scrollY;
+  if (prevScrollPos > currentScrollPos) {
+    navbar.style.top = "0";
+  } else {
+    navbar.style.top = "-60px"; // Adjust this value to hide the navbar completely
+  }
+  prevScrollPos = currentScrollPos;
+}
+if (
+  window.matchMedia("(min-width: 769px) and (max-width: 1024px)").matches ||
+  window.matchMedia("(max-width: 768px)").matches
+) {
+  window.addEventListener("scroll", handleScroll);
+} else {
+  // Remove event listener for other screen sizes
+  window.removeEventListener("scroll", handleScroll);
+}
