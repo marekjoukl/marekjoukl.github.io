5f349a03d25d5c69b1cc64cbcf13570ffdbc236a
Marek Joukl <jouklmarek@seznam.cz>
2024-04-26 13:57:49 +0200

    feat: closing sidebar when clicking elsewhere

--- js/script.js
+++ js/script.js
@@ -94,12 +94,27 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 // Sidebar script
+let sidemenu;
+
+document.addEventListener("DOMContentLoaded", function () {
+  sidemenu = document.getElementById("sidemenu");
+
+  // Add event listener to close the sidebar when clicking anywhere on the document
+  document.addEventListener("click", function (event) {
+    // Check if the clicked element is inside the sidebar
+    if (
+      !sidemenu.contains(event.target) &&
+      sidemenu.style.right == "0px" &&
+      event.target.id != "burger-menu"
+    ) {
+      closemenu();
+    }
+  });
+});
+
 function openmenu() {
   sidemenu.style.right = "0";
 }
 function closemenu() {
   sidemenu.style.right = "-220px";
 }
-document.addEventListener("DOMContentLoaded", function () {
-  let sidemenu = document.getElementById("sidemenu");
-});
