783e9779882aa72d80b2d86f5cab384cbdcafa30
Marek Joukl <jouklmarek@seznam.cz>
2024-04-26 22:37:21 +0200

    fix: navbar sidebar hide

--- js/hideNavbar.js
+++ js/hideNavbar.js
@@ -1,21 +1,24 @@
-const navbar = document.getElementById("nav");
-let prevScrollPos = window.scrollY;
+document.addEventListener("DOMContentLoaded", function () {
+  const navbar = document.getElementById("nav");
+  const sidemenu = document.getElementById("sidemenu");
+  let prevScrollPos = window.scrollY;
 
-function handleScroll() {
-  let currentScrollPos = window.scrollY;
-  if (prevScrollPos > currentScrollPos) {
-    navbar.style.top = "0";
-  } else {
-    navbar.style.top = "-70px"; // Adjust this value to hide the navbar completely
+  function handleScroll() {
+    let currentScrollPos = window.scrollY;
+    if (prevScrollPos > currentScrollPos) {
+      navbar.style.top = "0";
+    } else {
+      if (sidemenu.style.right != "0px") navbar.style.top = "-70px";
+    }
+    prevScrollPos = currentScrollPos;
   }
-  prevScrollPos = currentScrollPos;
-}
-if (
-  window.matchMedia("(min-width: 769px) and (max-width: 1024px)").matches ||
-  window.matchMedia("(max-width: 768px)").matches
-) {
-  window.addEventListener("scroll", handleScroll);
-} else {
-  // Remove event listener for other screen sizes
-  window.removeEventListener("scroll", handleScroll);
-}
+
+  if (
+    window.matchMedia("(min-width: 769px) and (max-width: 1024px)").matches ||
+    window.matchMedia("(max-width: 768px)").matches
+  ) {
+    window.addEventListener("scroll", handleScroll);
+  } else {
+    window.removeEventListener("scroll", handleScroll);
+  }
+});
